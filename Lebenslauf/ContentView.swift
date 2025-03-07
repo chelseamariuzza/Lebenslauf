a42742ff9472a280748e8f2f11f76284d9fcbc94
Charles Mariuzza <charles@mariuzza.de>
2025-03-07 13:32:32 +0000

    Initial commit
    First steps creating resumee

---
+++ Lebenslauf/ContentView.swift
@@ -0,0 +1,64 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Image(.pacman)
+                .resizable()
+                .scaledToFill()
+                .clipped()
+                .ignoresSafeArea()
+                .overlay{
+                    VStack{
+                        Image(.profilePicture)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 290)
+                        
+                        Text("Charles Mariuzza")
+                            .font(.title)
+                            .foregroundColor(.white)
+                    }
+                    .padding(.bottom)
+                }
+            
+            VStack{
+                HStack() {
+                    Text("Berufserfahrung:")
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                        Text("Ausbildung Tyre24 \n 2015-2018 \n\n SAITOW AG \n 2018-2019 \n\n C24 \n 2019-2024")
+                            .multilineTextAlignment(.center)
+                            .padding(.trailing, 32)
+                }
+                .padding(.bottom, 5)
+                
+                HStack {
+                    Text("Kenntnnisse:")
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    Text("Android Entwicklung \n Java + Kotlin \n Firebase \n Dependency Injection, Junit")
+                        .multilineTextAlignment(.center)
+                }
+            }
+            
+            Button("Kontakt", action: navigationToKontakt)
+                .padding(10)
+            
+        }
+        .padding(.leading, 16)
+        .padding(.trailing, 16)
+    }
+}
+
+func navigationToKontakt (){
+    //todo
+}
+
+#Preview {
+    ContentView()
+}
