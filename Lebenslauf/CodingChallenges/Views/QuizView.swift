53c59faedff1cae142c32f5b4cbe834c2fad1616
Charles Mariuzza <charles@mariuzza.de>
2025-03-12 12:17:25 +0100

    3-01.3 Coding Challenge 2 #253
    - Adds QuizView

---
+++ Lebenslauf/CodingChallenges/Views/QuizView.swift
@@ -0,0 +1,20 @@
+//
+//  QuizView.swift
+//  Lebenslauf
+//
+//  Created by Charles Mariuzza on 12.03.25.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        Button("Melden") {
+            print("Schüler hat sich gemeldet")
+        }
+    }
+}
+
+#Preview {
+    QuizView()
+}
